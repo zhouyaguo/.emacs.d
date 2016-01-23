ec6331e38a6837c7533a05664c7b507dc983e1cb
Yaguo Zhou <zhouyaguo@unionpay.com>
2016-01-23 13:53:48 +0800

    http -> https

--- init.el
+++ init.el
@@ -3,7 +3,7 @@
 
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")
+                         ("melpa" . "https://melpa.milkbox.net/packages/")
 			 ("elpy" . "https://jorgenschaefer.github.io/packages/")
                          ))
 
