a1eb81486bfee693e50c26cf437def7f11966c59
Yaguo Zhou <zhouyaguo@gmail.com>
2016-03-25 13:36:59 +0800

    polish

--- init.el
+++ init.el
@@ -73,7 +73,7 @@
 (elpy-enable)
 
 ; markdown
-(autoload 'markdown-mode "markdown-mode" "Major mode for editing Markdown files" t)                                                          (add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))                                                                            
+(autoload 'markdown-mode "markdown-mode" "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))                                                                        
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
